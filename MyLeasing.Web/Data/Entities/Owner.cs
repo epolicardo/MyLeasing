dfa73d8433bdd25e770b319db8c3b7afaee43746
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-08-20 21:27:39 -0300

    Primer migracion lista

--- MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyLeasing.Web.Data.Entities
 {
     public class Owner
     {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+        public string Address { get; set; }
+        //Propiedades de solo lectura - No Mapean en DB
+        public string FullName => $"{FirstName} {LastName}";
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
     }
 }
--- MyLeasing.Web/Startup.cs
+++ MyLeasing.Web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyLeasing.Web.Data;
 
 namespace MyLeasing.Web
 {
@@ -30,7 +32,12 @@ namespace MyLeasing.Web
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            services.AddDbContext<DataContext>(cfg =>
+                {
+                    cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                });
 
+            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
